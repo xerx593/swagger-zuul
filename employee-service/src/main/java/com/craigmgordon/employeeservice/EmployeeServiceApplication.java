3951e5431ac86caba0e499ca71276f53548f06ab
xerx593 <alexlutz79@gmail.com>
2023-04-21 21:18:36 +0200

    fix by "modification of operation path"

--- employee-service/src/main/java/com/craigmgordon/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/craigmgordon/employeeservice/EmployeeServiceApplication.java
@@ -1,8 +1,12 @@
 package com.craigmgordon.employeeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.PathProvider;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,17 +18,41 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class EmployeeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
 
-	
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()  
-          .apis(RequestHandlerSelectors.basePackage(("com.craigmgordon.employeeservice")))            
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathProvider(new PathProvider() {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return "/unknownFunction2/"; // ??
+                    }
+
+                    @Override
+                    public String getOperationPath(String s) {
+                        return s.replace("/employees", contextPath() + "employees");
+                    }
+
+                    @Override
+                    public String getResourceListingPath(String s, String s1) {
+                        return "/unknownFunction3/"; // ??
+                    }
+                })
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(("com.craigmgordon.employeeservice")))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Autowired
+    ServerProperties serverProps;
+    @Autowired
+    WebMvcProperties webMvcProps;
+
+    private String contextPath() {
+        return serverProps.getServlet().getContextPath() + webMvcProps.getServlet().getPath();
     }
 }
--- product-service/src/main/java/com/craigmgordon/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/craigmgordon/productservice/ProductServiceApplication.java
@@ -1,9 +1,13 @@
 package com.craigmgordon.productservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.PathProvider;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -14,17 +18,41 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
-	
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()  
-          .apis(RequestHandlerSelectors.basePackage(("com.craigmgordon.productservice")))            
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathProvider(new PathProvider() {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return "/unknownFunction1/"; // ??
+                    }
+
+                    @Override
+                    public String getOperationPath(String s) { // this seems to have effect:
+                        return s.replace("/products", contextPath() + "products");
+                    }
+
+                    @Override
+                    public String getResourceListingPath(String s, String s1) {
+                        return "/unknownFunction2/"; // ???
+                    }
+                })
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(("com.craigmgordon.productservice")))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Autowired
+    ServerProperties serverProps;
+    @Autowired
+    WebMvcProperties webMvcProps;
+
+    private String contextPath() {
+        return serverProps.getServlet().getContextPath() + webMvcProps.getServlet().getPath();
+    }
 }
